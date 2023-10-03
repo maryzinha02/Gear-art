fcb91926c3a0541f671a228dcf5880f28bb2fe2d
Maria Eduarda Minto dos Santos <128842043+maryzinha02@users.noreply.github.com>
2023-10-03 19:23:28 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -14,18 +14,29 @@ $route->group(null);
 
 $route->get("/", "Web:home");
 $route->get("/sobre", "Web:about");
-$route->get("/registro","Web:register");
-$route->get("/contato","Web:contact");
+$route->get("/cadastro","Web:register");
+$route->get("/login","Web:login");
+$route->get("/carrinho","Web:shopCart");
+$route->get("/favoritos","Web:favorite");
 $route->get("/faq","Web:faq");
-$route->get("/conta","Web:account");
-$route->get("/pinturas","Web:arts");
-$route->get("/artistas","Web:artists");
+
+//rota teste
+$route->get("/mo", "Web:apiFaqs");
+
+$route->get("/catalogo","Web:art");
+$route->get("/catalogo/{author}","Web:art");
 
 $route->get("/ops/{errcode}", "Web:error");
 
 $route->group("/app");
 $route->get("/", "App:home");
+$route->get("/perfil", "App:profile");
+$route->group(null);
 
+
+$route->group("/admin");
+$route->get("/", "Adm:home");
+$route->get("/curadores", "Adm:curators");
 $route->group(null);
 
 $route->dispatch();
